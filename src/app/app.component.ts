1fddd9180b844f16117a2e0519d95e71e3f7794f
June Muoti <june.muoti@gmail.com>
2018-01-05 17:45:36 +0300

    add an array for goals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes';
+  quotes:string[];
+   constructor (){
+     this.quotes=["Time wasted can never be recovered","better late than never","I have so much to do that i have to spent the first few hours in prayer"];
+   }
 }
