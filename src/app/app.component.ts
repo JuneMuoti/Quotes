47231b1497aac7699cd783e8ae993937fed60df9
June Muoti <june.muoti@gmail.com>
2018-01-05 19:03:29 +0300

    add a goal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-
+import {Quote} from './quote'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
-   constructor (){
-     this.quotes=["Time wasted can never be recovered",
-     "Better late than never",
-     "I have so much to do that i have to spent the first few hours in prayer"
+  quotes =[
+    new Quote(1,"Time wasted can never be recovered"),
+     new Quote(2,"Better late than never"),
+     new Quote(3,"I have so much to do that i have to spent the first few hours in prayer")
    ];
    }
-}
