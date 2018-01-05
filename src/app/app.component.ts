60246fdae23abcdcb1dc707bb747838817a9cc2b
June Muoti <june.muoti@gmail.com>
2018-01-05 19:09:42 +0300

    move logic and html to goal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import {Quote} from './quote'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes =[
-    new Quote(1,"Time wasted can never be recovered"),
-     new Quote(2,"Better late than never"),
-     new Quote(3,"I have so much to do that i have to spent the first few hours in prayer")
-   ];
+
    }
