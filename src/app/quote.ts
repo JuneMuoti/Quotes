a626476cd7a781c18cb9a14b956729c44396dd4a
June Muoti <june.muoti@gmail.com>
2018-01-06 16:59:30 +0300

    add a pipe for date created

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showMeaning:boolean;
-  constructor(public id:number,public name:string, public meaning:string){
+  constructor(public id:number,public name:string, public meaning:string,public dateCreated:Date){
     this.showMeaning=false
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,9 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1,"Time wasted can never be recovered","manage your time well,it is a precious commodity"),
-     new Quote(2,"Better late than never","what matters us you made it"),
-     new Quote(3,"I have so much to do that i have to spent the first few hours in prayer","God above all")
+    new Quote(1,"Time wasted can never be recovered","manage your time well,it is a precious commodity",new Date(2018,1,1)),
+     new Quote(2,"Better late than never","what matters us you made it",new Date(2018,4,1)),
+     new Quote(3,"I have so much to do that i have to spent the first few hours in prayer","God above all",new Date(2017,12,12))
    ];
 toogleMeaning(index){
   this.quotes[index].showMeaning = !this.quotes[index].showMeaning;
